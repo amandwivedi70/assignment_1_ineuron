1ccee393f5a3b684ef0e4a0315eb80f7735249e5
unknown <amandwivedi70@gmail.com>
2022-09-08 13:32:45 +0530

    upload

---
+++ practice_assign_1.py
@@ -0,0 +1,35 @@
+# question1
+import sys
+print(sys.version)
+
+# question2
+print("hello python")
+
+# question3
+print("hello")
+print("python")
+
+# another method
+print("hello", "python",sep='\n')
+
+# question4
+print("\"MySirG\"")
+
+# question5
+print("\"teacher's day\"")
+
+# question8
+x="aman dwivedi"
+print(x)
+# question9
+a="aman dwivedi"
+b="28"
+c="btech"
+d="0"
+print(a,b,c,d,sep="\n")
+
+# question10
+a=10
+b=20
+c=30
+print(a,b,c,sep="\n")
